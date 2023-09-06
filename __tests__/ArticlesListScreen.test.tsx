ec8146a5a6eeb81d542ea12c866d3d1c0ae57bab
Michel Azzam <michelazzam@Michels-MacBook-Pro.local>
2023-09-06 22:46:23 +0300

    cleanup

--- __tests__/ArticlesListScreen.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-// import ArticlesListView from '../src/views/ArticlesListView';
-import App from "../src/App";
-
-test("renders correctly", () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
